774a28472fb5928caabe032e3b47c9f900f62ac7
gestertim <gester.tim@gmail.com>
2026-08-14 07:42:26 +0800

    fix: route home to app entry

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
+import { redirect } from "next/navigation";
+
 export default function HomePage() {
-  return (
-    <main>
-      <h1>Go Anywhere</h1>
-      <p>讓你的行程一目了然。</p>
-    </main>
-  );
+  redirect("/explore");
 }
